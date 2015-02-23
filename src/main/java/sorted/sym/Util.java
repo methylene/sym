ccaa62d296cf6c134f69e3edd611bac54a27c80a
lbohl <lbohl@expedia.com>
2015-02-23 14:15:34 +0000

    center, cartesian, commutator

--- src/main/java/sorted/sym/Util.java
+++ src/main/java/sorted/sym/Util.java
@@ -2,7 +2,9 @@ package sorted.sym;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
@@ -74,24 +76,25 @@ class Util {
   static class State {
     final int[] prefix;
     final int[] suffix;
+
     State(int[] prefix, int[] suffix) {
       this.prefix = prefix;
       this.suffix = suffix;
     }
   }
 
-  static List<int[]> permutations(int n) {
+  static List<Permutation> permutations(int n) {
     int[] start = new int[n];
     for (int i = 0; i < n; i += 1) {
       start[i] = i + 1;
     }
     Stack<State> stack = new Stack<State>();
     stack.push(new State(new int[0], start));
-    List<int[]> result = new LinkedList<int[]>();
+    List<Permutation> result = new LinkedList<Permutation>();
     while (!stack.isEmpty()) {
       State state = stack.pop();
       if (state.suffix.length == 0) {
-        result.add(state.prefix);
+        result.add(Permutation.perm1(state.prefix));
       } else {
         for (int i = 0; i < state.suffix.length; i += 1) {
           int[] newPrefix = new int[state.prefix.length + 1];
@@ -105,6 +108,83 @@ class Util {
           stack.push(new State(newPrefix, newSuffix));
         }
       }
+    }
+    return result;
+  }
+
+  static List<Permutation> toPermutations(List<int[]> input) {
+    ArrayList<Permutation> result = new ArrayList<Permutation>(input.size());
+    for (int[] ints : input)
+      result.add(Permutation.perm1(ints));
+    return result;
+  }
+
+  static Iterable<Permutation[]> cartesian(final List<Permutation> a, final List<Permutation> b) {
+    return new Iterable<Permutation[]>() {
+      @Override
+      public Iterator<Permutation[]> iterator() {
+        return new Iterator<Permutation[]>() {
+          int idxa = 0;
+          int idxb = 0;
+
+          @Override
+          public boolean hasNext() {
+            return idxa < a.size();
+          }
+
+          @Override
+          public Permutation[] next() {
+            Permutation pa = a.get(idxa);
+            Permutation pb = b.get(idxb);
+            if (b.size() - idxb == 1) {
+              idxb = 0;
+              idxa += 1;
+            } else {
+              idxb += 1;
+            }
+            return new Permutation[]{pa, pb};
+          }
+
+          @Override
+          public void remove() {
+            throw new IllegalAccessError();
+          }
+        };
+      }
+    };
+  }
+
+  public static Iterable<Permutation> commutator(final Collection<Permutation> input) {
+    List<Permutation> inlist = Arrays.asList(input.toArray(new Permutation[input.size()]));
+    final Iterator<Permutation[]> cartesian = cartesian(inlist, inlist).iterator();
+    return new Iterable<Permutation>() {
+      @Override
+      public Iterator<Permutation> iterator() {
+        return new Iterator<Permutation>() {
+          @Override
+          public boolean hasNext() {
+            return cartesian.hasNext();
+          }
+          @Override
+          public Permutation next() {
+            Permutation[] p = cartesian.next();
+            return Permutation.prod(p[0].invert(), p[1].invert(), p[0], p[1]);
+          }
+          @Override
+          public void remove() {
+            throw new IllegalAccessError();
+          }
+        };
+      }
+    };
+  }
+
+  public static List<Permutation> center(final Collection<Permutation> input) {
+    LinkedList<Permutation> result = new LinkedList<Permutation>();
+    outer: for (Permutation a: input) {
+      for (Permutation b: input)
+        if (!a.comp(b).equals(b.comp(a))) continue outer;
+      result.add(a);
     }
     return result;
   }
--- src/test/java/sorted/sym/UtilTest.java
+++ src/test/java/sorted/sym/UtilTest.java
@@ -1,27 +1,59 @@
 package sorted.sym;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import static org.junit.Assert.*;
 
 public class UtilTest {
 
   @Test
   public void testCombinations() throws Exception {
-    List<int[]> permutations = Util.permutations(3);
+    List<Permutation> permutations = Util.permutations(3);
     assertEquals(6, permutations.size());
     Set<Permutation> perms = new HashSet<Permutation>();
-    for (int[] ints: permutations) {
-      assertTrue(perms.add(Permutation.perm1(ints)));
-      assertEquals(3, ints.length);
+    for (Permutation perm: permutations) {
+      assertTrue(perms.add(perm));
     }
-    for (int[] ints: Util.permutations(3)) {
-      assertFalse(perms.add(Permutation.perm1(ints)));
+    for (Permutation perm: Util.permutations(3)) {
+      assertFalse(perms.add(perm));
     }
   }
+
+  @Test
+  public void testCartesian() throws Exception {
+    int total = 0;
+    int offDiagonal = 0;
+    List<Permutation> a = Util.permutations(3);
+    for (Permutation[] permutation: Util.cartesian(a, a)) {
+      total += 1;
+      if (permutation[0] != permutation[1]) {
+        offDiagonal += 1;
+      }
+    }
+    assertEquals(36, total);
+    assertEquals(30, offDiagonal);
+  }
+
+  @Test
+  public void testCenter() throws Exception {
+    List<Permutation> a = Util.permutations(5);
+    List<Permutation> center = Util.center(a);
+    assertEquals(1, center.size());
+    assertEquals(Permutation.IDENTITY, center.get(0));
+  }
+
+  @Test
+  public void testCommutator() throws Exception {
+    Iterable<Permutation> commutator = Util.commutator(Util.permutations(5));
+    HashSet<Permutation> commutatorSet = new HashSet<Permutation>();
+    for (Permutation p: commutator)
+      commutatorSet.add(p);
+    assertEquals(60, commutatorSet.size());
+  }
+
 }
