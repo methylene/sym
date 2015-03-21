10867d55d64972604a1e4767f11ce2c747c5c63c
lbohl <lbohl@expedia.com>
2015-03-21 15:27:12 +0000

    add package info

---
+++ sym/src/main/java/com/github/methylene/lists/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package defines the abstract class LookupList and various concrete subclasses.
+ * These are immutable lists that are optimized for searching.
+ */
+package com.github.methylene.lists;
--- sym/src/main/java/com/github/methylene/sym/package-info.java
+++ sym/src/main/java/com/github/methylene/sym/package-info.java
@@ -1,2 +1,4 @@
-/** Permutations for Java */
+/**
+ * This package provides basic permutation operations.
+ */
 package com.github.methylene.sym;
